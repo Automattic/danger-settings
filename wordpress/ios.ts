3b4edc73ad12c0c3e75b221494a0777839c47887
Gio Lodi <giovanni.lodi42@gmail.com>
2020-05-27 16:44:03 +1000

    Add check for diff size to WordPress iOS

--- wordpress/ios.ts
+++ wordpress/ios.ts
@@ -3,9 +3,11 @@ export async function wordPressIOS () {
   // picked up by the imports resolver Danger uses with remote
   const {checkLabel} = await import("../org/label")
   const {checkMilestone} = await import("../org/milestone")
+  const {checkDiffSize} = await import("../org/pr/diff-size")
   const {iOSSafetyChecks} = await import("../org/pr/ios")
 
   await checkLabel()
+  await checkDiffSize()
   // This is disabled because it needs more refined event handling on the
   // GitHub Actions workflow side.
   // See https://github.com/wordpress-mobile/WordPress-iOS/pull/14187
