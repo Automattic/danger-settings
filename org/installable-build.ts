164c6f1162e619287aae5fa9a7220a2e256f8743
Gio Lodi <giovanni.lodi42@gmail.com>
2020-05-28 15:12:15 +1000

    Start porting over the logic for installable builds

---
+++ org/installable-build.ts
@@ -0,0 +1,26 @@
+import {Status} from "github-webhook-event-types"
+// Danger ignores Danger's imports at runtime and actually would give a build
+// failure if we tried to use them here and then import this file. What authors
+// should do, instead, is simply assume it's there.
+//
+// See https://danger.systems/js/usage/extending-danger.html#writing-your-plugin
+//
+// In order to write tests for these files, though, we need to define
+// interfaces for the Danger functionalities we'll use. These don't affect
+// runtime and do provide better dev-time typing experience.
+//
+// A richer example of this can be found here:
+// https://github.com/orta/danger-plugin-yarn/blob/5e906e57331b952afced3f6e773ed6c68d5898c0/src/index.ts#L1-L8
+import { DangerDSLType } from "../node_modules/danger/distribution/dsl/DangerDSL"
+declare var danger: DangerDSLType
+export declare function warn(message: string): void
+
+export async function installableBuild(status: Status) {
+  console.log("Not a status we want to process for installable builds - got 'Failure context' (fail)")
+};
+
+// Not exactly sure why, but in order for the multiple files + import setup to
+// work we need to split the export of this function and its declaration as the
+// default export. I'm guessing it has to do with how TypeScript resolves
+// these?
+export default installableBuild
---
+++ tests/installable-build.test.ts
@@ -0,0 +1,34 @@
+import {installableBuild} from "../org/installable-build";
+
+// Because Danger strips Danger's imports at runtime, we don't import Danger in
+// our sources. Instead, we simply assume they exist in the global namespace.
+// In the tests, we need to provide a test double for everything, though.
+declare const global: any
+
+beforeEach(() => {
+  global.console = {
+    log: jest.fn()
+  }
+  global.warn = jest.fn()
+  global.danger = {
+    github: {
+      issue: {
+        labels: []
+      }
+    }
+  }
+})
+
+describe("installable build handling", () => {
+  it("bails when it's not a status/state we want to handle", async () => {
+    // The original source casts as any, I'm guessing to work around the need to add extra information to the typed value.
+    // Argument of type     '{ state: string; context: string; }' is not assignable to parameter of type              'Status'. Type '{ state: string; context: string; }' is missing the following             properties from type 'Status': id, sha, name, commit, and 5 more.
+    // Later on, I'd like to do something about this, for example by creating a
+    // builder function in this test for it.
+    await installableBuild({ state: "fail", context: "Failure context" } as any);
+
+    expect(console.log).toBeCalledWith(
+      "Not a status we want to process for installable builds - got 'Failure context' (fail)"
+    )
+  })
+})
