b6b68d330013b1f280bacc9d6983c6613a5e2864
Gio Lodi <giovanni.lodi42@gmail.com>
2020-06-17 15:06:56 +1000

    Add Dangerfile for WordPress Android specific checks

---
+++ wordpress/android.ts
@@ -0,0 +1,19 @@
+export async function wordPressAndroid () {
+  // The imports _need_ to be done within this async function
+  // picked up by the imports resolver Danger uses with remote
+  const {checkLabel} = await import("../org/label")
+  const {checkMilestone} = await import("../org/pr/milestone")
+  const {checkDiffSize} = await import("../org/pr/diff-size")
+  const {androidSafetyChecks} = await import("../org/pr/android")
+
+  await checkLabel()
+  await checkDiffSize()
+  // This is disabled because it needs more refined event handling on the
+  // GitHub Actions workflow side.
+  // See https://github.com/wordpress-mobile/WordPress-iOS/pull/14187
+  // await checkMilestone()
+  await androidSafetyChecks()
+  // TODO: need to port over check-subtrees, too
+}
+
+export default wordPressAndroid
